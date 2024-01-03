ad7f892de7ad29ece6729796ffa750615e64d8fd
NutchanonRungthong <anonimoseza@gmail.com>
2024-01-03 16:04:41 +0700

    update

--- index.js
+++ index.js
@@ -1,64 +1,75 @@
+//
+//
+//
+//
 require("dotenv").config();
 const express = require('express');
 const app = express();
 
+//
 app.use(express.json());
 
-let book = [
+//
+let books = [
     {
-        id:1,
-        title: 'book1',
-        author: 'Author1'
+        id: 1,
+        title: 'Book 1',
+        author: 'Author 1'
     },
     {
-        id:2,
-        title: 'book2',
-        author: 'Author2'
+        id: 2,
+        title: 'Book 2',
+        author: 'Author 2'
     },
     {
-        id:3,
-        title: 'book3',
-        author: 'Author3'
+        id: 3,
+        title: 'Book 3',
+        author: 'Author 3'
     }
 ];
 
-app.get('/book',(req,res) => {
+//
+app.get('/books', (rep, res) => {
     res.json(books);
 });
 
-app.get('/book/:id',(req,res)=>{
-    const book = books.find(b => b.id === parseInt (req.params.id));
-    if(!book) res.status(404).send('Book not found');
+//
+app.get('/books/ :id', (rep, res) => {
+    const book = books.find(b => b.id === parseInt(rep.params.id));
+    if (!book) res.status(404).send('Book not found');
     res.json(book);
 });
 
-app.post('/books',(req,res) =>{
-    const book ={
-        id:books.lenght +1,
-        title: req.body.title,
-        author: req.body.author
+//
+app.post('/books', (rep, res) => {
+    const book = {
+        id: books.length + 1,
+        title: rep.body.title,
+        author: rep.body.author 
     };
     books.push(book);
     res.send(book);
 });
 
-app.put('/books/:id',(req,res) => {
-    const book = books.find(b => b.id === parseInt(req.params.id));
+//
+app.put('/books/:id', (rep, res) => {
+    const book = books.find(b => b.id === parseInt(rep.params.id));
     if (!book) res.status(404).send('Book not found');
-    book.title = req.body.title;
-    book.author = req.bidy.author;
+    book.title = rep.body.title;
+    book.author = rep.body.author;
     res.send(book);
 });
 
-app.delete('/books/:id',(req,res) =>{
-    const book = books.find(b => b.id === parseInt(req.params,id));
-    if(!book) res.status(404).send('Book not found');
-    const index = book.indexOf(book);
-    book.splice(index,1);
+//
+app.delete('/books/:id', (rep, res) => {
+    const book = books.find(b => b.id === parseInt(rep.params.id));
+    if (!book) res.status(404).send('Book not found');
+    const index = books.indexOf(book);
+    books.splice(index, 1);//Delete index
     res.send(book);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
