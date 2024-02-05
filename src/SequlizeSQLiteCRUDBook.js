05f77bb1784f9ac16e1ea0f28375e27e1c2bf523
NutchanonRungthong <anonimoseza@gmail.com>
2024-02-05 10:45:06 +0700

    dd

--- src/SequlizeSQLiteCRUDBook.js
+++ src/SequlizeSQLiteCRUDBook.js
@@ -2,38 +2,34 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const app = express();
 
-
 app.use(express.json());
 
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-    host: 'locahost',
-    dialect: 'sqlite',
-    storage: './Database/SQBooks.sqlite'
+const sequelize = new Sequelize('database','username','password',{
+    host:'localhost',
+    dialect:'sqlite',
+    storage:'./Database/SQBooks.sqlite'
 });
 
-
-const Book = sequelize.define('book', {
-    id: {
+const Book = sequelize.define('book',{
+    id:{
         type: Sequelize.INTEGER,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey:true
     },
-    title: {
+    title:{
         type: Sequelize.STRING,
         allowNull: false
     },
-    author: {
+    author:{
         type: Sequelize.STRING,
         allowNull: false
     }
 });
 
-
 sequelize.sync();
 
-
-app.get('/books', (req, res) => {
+app.get('/books/:id',(req,res) => {
     Book.findAll().then(books => {
         res.json(books);
     }).catch(err => {
@@ -41,35 +37,32 @@ app.get('/books', (req, res) => {
     });
 });
 
-
-app.get('/books/:id', (req, res) => {
+app.get('/books/:id', (req,res) => {
     Book.findByPk(req.params.id).then(book => {
-        if (!book) {
+        if(!book){
             res.status(404).send('Book not found');
-        } else {
+        }else{
             res.json(book);
         }
+    }).catch(err =>{
+        res.status(500).send(err);
+    });
+});
+
+app.post('/books',(req,res) => {
+    Book.create(req.body).then(book => {
+        res.send(book);
     }).catch(err => {
         res.status(500).send(err);
     });
 });
 
-
-app.post('/books', (req, res) => {
-    Book.create(req.body).then(books => {
-        res.send(books);
-    }).catch(err => {
-        res.status(500).send(err);
-    });
-});
-
-
-app.put('/books/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        if (!book) {
+app.put('/books/:id',(req,res) =>{
+    Book.findByPk(req.params.id).then(books => {
+        if(!book){
             res.status(404).send('Book not found');
-        } else {
-            res.update(req.body).then(() => {
+        }else{
+            book.update(req.body).then(() => {
                 res.send(book);
             }).catch(err => {
                 res.status(500).send(err);
@@ -80,13 +73,12 @@ app.put('/books/:id', (req, res) => {
     });
 });
 
-
-app.delete('/books/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        if (!book) {
+app.delete('/books/:id',(req,res) => {
+    Book.findByPk(req.params.id).then(book =>{
+        if(!book){
             res.status(404).send('Book not found');
-        } else {
-            res.destroy().then(() => {
+        }else{
+            book.destroy().then(() => {
                 res.send({});
             }).catch(err => {
                 res.status(500).send(err);
@@ -97,6 +89,6 @@ app.delete('/books/:id', (req, res) => {
     });
 });
 
-
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on porthttp://localhost:${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+    
